5dbe83da72c0e98fa962f66d612d27f61bb1f600
hoarse5 <64092112+hoarse5@users.noreply.github.com>
2021-08-10 19:20:34 +0200

    v2: prices, tab toggle

--- script.js
+++ script.js
@@ -10,6 +10,25 @@ function pageLoad() {
             navbarHeader.classList.remove("colorChange-nav-span");
         }
     };
+
+    //tab toggle code source: https://www.youtube.com/watch?v=5L6h_MrNvsk&t=53s
+
+    let tabs = document.querySelectorAll("[data-tab-target]");
+    let tabContents = document.querySelectorAll("[data-tab-content]");
+
+    tabs.forEach((tab) => {
+        tab.addEventListener("click", () => {
+            let target = document.querySelector(tab.dataset.tabTarget);
+            tabContents.forEach((tabContent) => {
+                tabContent.classList.remove("active");
+            });
+            tabs.forEach((tab) => {
+                tab.classList.remove("active");
+            });
+            tab.classList.add("active");
+            target.classList.add("active");
+        });
+    });
 }
 
 window.addEventListener("load", pageLoad);
